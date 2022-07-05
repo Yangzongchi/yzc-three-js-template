2fb9b3ce1bd9c4950b9202e1afaf696768e2ee52
ZongChi Yang <2607592114@qq.com>
2022-07-05 18:00:03 +0800

    finished initialization

---
+++ sources/Experience/assets.js
@@ -0,0 +1,10 @@
+export default [
+    {
+        name: 'base',
+        data: {},
+        items:
+        [
+            { name: 'bstTexture', source: '/assets/bst-flag.png', type: 'texture' },
+        ]
+    }
+]
